11304137d419641dfc81fc1aa8db30ee3909ac4c
antariksh39 <gosain.antariksh@gmail.com>
2023-08-15 03:23:20 +0530

    meals - safe area added in stack

--- meals/App.js
+++ meals/App.js
@@ -2,13 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar backgroundColor = "#b3e6ff"  bar-style='auto' />
+      <StatusBar bar-style='light' />
       <NavigationContainer>
-        <CategoriesScreen/>
+        <Stack.Navigator>
+          <Stack.Screen name="Meals Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
+
+        {/* Note needed here because component is provided in Stack.screen */}
+        {/* <CategoriesScreen/> */}
       </NavigationContainer>
     </>
   );
