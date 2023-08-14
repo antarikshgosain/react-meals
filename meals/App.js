7304950a8e0601c6710159ff76e8df89a610927f
antariksh39 <gosain.antariksh@gmail.com>
2023-08-15 03:13:04 +0530

    meals - navigation added

--- meals/App.js
+++ meals/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <>
-      <StatusBar backgroundColor = "#b3e6ff"   bar-style='auto' />
-      <CategoriesScreen/>
+      <StatusBar backgroundColor = "#b3e6ff"  bar-style='auto' />
+      <NavigationContainer>
+        <CategoriesScreen/>
+      </NavigationContainer>
     </>
   );
 }
