52e3a782bf0cfe6cd1a3fdcc47d759950672c1cc
antariksh39 <gosain.antariksh@gmail.com>
2023-08-29 18:26:07 +0530

    meals overview screen complete with image and some details

--- meals/components/MealItem.js
+++ meals/components/MealItem.js
@@ -1,11 +1,69 @@
-import { View, Text, StyleSheet, FlatList, } from 'react-native'
+import { View, Text, StyleSheet, Image, Pressable, FlatList, Platform } from 'react-native'
 
-function MealItem( {title} ) {
+function MealItem( {title, imageUrl, duration, complexity, affordability} ) {
     return (
         <View>
-            <Text>{title}</Text>
+            <Pressable>
+                <View style={styles.innerContainer}>
+                    <View style={styles.mealItem}>
+                        <Image source={{ uri: imageUrl }} style={styles.image} />
+                        
+                        <Text style={styles.title}> {title} </Text>
+                        <View style={styles.details}>
+                            <Text style={styles.detailItem} > {duration} minutes</Text>
+                            <Text style={styles.detailItem} > {complexity.toUpperCase()} </Text>
+                            <Text style={styles.detailItem} > {affordability.toUpperCase()} </Text>
+                        </View>
+                    </View>
+                </View>
+            </Pressable>
         </View>
     )
 }
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+    mealItem: {
+        margin: 16,
+        borderRadius: 8,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+
+        //shadown for android
+        elevation: 4,
+        overflow: Platform.OS == 'android' ? 'hidden' : 'visible',
+
+        //shadow for iOS
+        backgroundColor: 'white',
+        shadowColor: '#2d2626',
+        shadowOffset: { width: 1, height: 1 },
+        shadowOpacity: 0.4,
+        shadowRadius: 3,
+    },
+    innerContainer: {
+        overflow: 'hidden',
+        borderRadius: 10,
+    },
+    image: {
+        width: '100%',
+        height: 200,
+        borderRadius: 8
+    },
+    title: {
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fonctSize: 20,
+        margin: 8,
+    },
+    details: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8
+    },
+    detailItem: {
+        marginHorizontal: 4,
+        fontSize: 16
+    }
+});
--- meals/screens/MealsOverviewScreen.js
+++ meals/screens/MealsOverviewScreen.js
@@ -4,26 +4,35 @@ import { View, Text, StyleSheet, FlatList, } from 'react-native'
 import MealItem from "../components/MealItem";
 
 
-function MealsOverviewScreen({ navigation, route }){
+function MealsOverviewScreen({ navigation, route }) {
 
     const categoryId = route.params.categoryId;
     const categoryColor = route.params.categoryColor;
     const categoryTitle = route.params.categoryTitle;
 
-    const displayedMeals = MEALS.filter( (mealItem) => {
-        return mealItem.categoryIds.indexOf(categoryId) >= 0 ;
-    } );
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(categoryId) >= 0;
+    });
 
     function renderMealItem(itemData) {
+        const item = itemData.item;
+
+        const mealItemProp = {
+            title: item.title,
+            imageUrl: item.imageUrl,
+            affordability: item.affordability,
+            complexity: item.complexity,
+            duration: item.duration,
+        };
         return (
-            <MealItem title={itemData.item.title}></MealItem>
+            <MealItem {...mealItemProp} />
         )
     }
 
     return (
         <View style={styles.container}>
             <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem}>
-                
+
             </FlatList>
         </View>
     )
