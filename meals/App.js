50187bd52ef60271beb5ec2e14879528d7cb6447
antariksh39 <gosain.antariksh@gmail.com>
2023-09-05 01:54:32 +0530

    app.js formatted - no code change

--- meals/App.js
+++ meals/App.js
@@ -9,30 +9,30 @@ import MealDetailsScreen from './screens/MealDetailsScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <>
-      <StatusBar style='light' />
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-        headerStyle: {backgroundColor: '#683216'},
-        headerTintColor: 'white',
-        contentStyle: {backgroundColor: '#988e88'}}}>
-          <Stack.Screen name="MealsCategories" component={CategoriesScreen} options={{
-            title: 'All Categories',
-          }}/>
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen name="MealDetailsScreen" component={MealDetailsScreen} />
-        </Stack.Navigator>
+    return (
+        <>
+            <StatusBar style='light' />
+            <NavigationContainer>
+                <Stack.Navigator screenOptions={{
+                    headerStyle: { backgroundColor: '#683216' },
+                    headerTintColor: 'white',
+                    contentStyle: { backgroundColor: '#988e88' }}}>
+                    <Stack.Screen name="MealsCategories" component={CategoriesScreen} options={{
+                        title: 'All Categories',
+                    }} />
+                    <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+                    <Stack.Screen name="MealDetailsScreen" component={MealDetailsScreen} />
+                </Stack.Navigator>
 
-        {/* Note needed here because component is provided in Stack.screen */}
-        {/* <CategoriesScreen/> */}
-      </NavigationContainer>
-    </>
-  );
+                {/* Note needed here because component is provided in Stack.screen */}
+                {/* <CategoriesScreen/> */}
+            </NavigationContainer>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
+    container: {
 
-  },
+    },
 });
